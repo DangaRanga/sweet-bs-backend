b93ce8b54929b4fd6ee5b59894d33140c6f731d0
DangaRanga <jasonblue1626@gmail.com>
2021-02-09 23:15:21 -0500

    Begun setting up REST API

--- src/config/config.py
+++ src/config/config.py
@@ -3,21 +3,23 @@
 import os
 
 # Flask imports
-from flask import Flask
 
 
-class FlaskApp(Flask):
-    """Primary class for the flask application."""
+class Config():
+    """Primary class for configuration."""
 
-    def __init__(self, import_name):
-        """Initialize the Flask App."""
-        # Inherit class methods and attributes from super class
-        super(FlaskApp, self).__init__(import_name)
+    CSRF_ENABLED = True
+    SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URI")
+    JWT_ACCESS_LIFESPAN = {'hours': 24}
+    JWT_REFRESH_LIFESPAN = {'days': 30}
 
-        # Update Configuration values
-        self.config.update(
-            SECRET_KEY=os.environ.get("JWT_SECRET_KEY"),
-            JWT_ACCESS_LIFESPAN=os.environ.get("ACCESS_LIFESPAN"),
-            JWT_REFRESH_LIFESPAN=os.environ.get("REFRESH_LIFESPAN"),
-            SQLALCHEMY_DATABASE_URI=os.environ.get("DB_URI")
-        )
+    def __init__(self, debug_mode=True):
+        """Initialize class."""
+        self.debug_mode = debug_mode
+
+
+app_conig = {
+    'development': Config(),
+    'production': Config(False)
+}
--- src/server.py
+++ src/server.py
@@ -1,6 +1,47 @@
 """Primary module for running the API."""
 
-from config.config import FlaskApp
+# Flask imports
+from flask import Flask
+
+# User module imports
+from api.views.views import UserAPI, OrdersAPI, IngredientsAPI
+
+
+class FlaskApp(Flask):
+
+    def __init__(self, import_name):
+        # Inherit class methods from Flask
+        super(FlaskApp, self).__init__(import_name)
+        self.register_apis()
+
+    def register_api(self, view, endpoint, url, p_key='id', p_key_type='int'):
+        """Generalized method for registering new views/API endpoints
+
+        Args:
+            view: The MethodView object
+            endpoint: The endpoint for the api
+
+        """
+        view_function = view.as_view(endpoint)
+
+        # Setting up URL rule for GET Method with priamry key
+        self.add_url_rule(
+            url, defaults={p_key: None},
+            view_func=view_function, methods=['GET'])
+
+        # Setting up URL rule for POST Method
+        self.add_url_rule(url, view_func=view_function, methods=['POST'])
+
+        # Setting up URL Rule for GET, POST and DELETE
+        self.add_url_rule(f'{url} {p_key_type} {p_key}',
+                          view_func=view_function,
+                          methods=['GET', 'DELETE', 'PUT'])
+
+    def register_apis(self):
+        view_lst = [UserAPI, OrdersAPI, IngredientsAPI]
+        for view in view_lst:
+            self.register_api(view, view.endpoint, view.url, view.p_key)
+
 
 if __name__ == '__main__':
     app = FlaskApp(__name__)
